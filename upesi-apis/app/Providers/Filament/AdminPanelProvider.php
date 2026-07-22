365af83435fb16353d0337562751ddb1c1f5196d
DOSSE-Kovi-Amen <koviamendosse3@gmail.com>
2026-07-22 19:42:34 +0100

    Dernière mise à jour pour upesi

--- upesi-apis/app/Providers/Filament/AdminPanelProvider.php
+++ upesi-apis/app/Providers/Filament/AdminPanelProvider.php
@@ -29,7 +29,7 @@ class AdminPanelProvider extends PanelProvider
         return $panel
             ->default()
             ->id('admin')
-            ->path('admin')
+            ->path('admin') // Active le menu horizontal
             ->sidebarCollapsibleOnDesktop() // Ajoute cette ligne
             ->userMenuItems([
                 Action::make('wallet')
